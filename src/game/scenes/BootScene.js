0261b409af3bb8ae428e62574f6ac6cb89b7f977
Ignacio Martín <natx.mc@gmail.com>
2019-03-11 16:16:55 +0100

    Massive update to upgrade to webpack 4 and get rid of *.vue files. Template loading rearranged with raw-loader. Asset loading also modified

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,22 +1,19 @@
-import { Scene } from 'phaser'
-import sky from '@/game/assets/sky.png'
-import bomb from '@/game/assets/bomb.png'
-import thudMp3 from '@/game/assets/thud.mp3'
-import thudOgg from '@/game/assets/thud.ogg'
-
+import {Scene} from 'phaser'
+import sky from '@/game/assets/sky.png';
+import bomb from '@/game/assets/bomb.png';
 
 export default class BootScene extends Scene {
-  constructor () {
-    super({ key: 'BootScene' })
-  }
+    constructor() {
+        super({key: 'BootScene'})
+    }
 
-  preload () {
-    this.load.image('sky', sky)
-    this.load.image('bomb', bomb)
-    this.load.audio('thud', [thudMp3, thudOgg])
-  }
+    preload() {
+        this.load.image('sky', sky)
+        this.load.image('bomb', bomb)
+        // this.load.audio('thud', ['assets/thud.mp3', 'assets/thud.ogg'])
+    }
 
-  create () {
-    this.scene.start('PlayScene')
-  }
+    create() {
+        this.scene.start('PlayScene')
+    }
 }
